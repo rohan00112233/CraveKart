8c1a56a9b471b0c65b5f18c45f8d4cbfee1072ea
rohan00112233 <microsoftrohan1234@gmail.com>
2026-06-08 20:30:56 +0530

    Added MySQL integration, restaurant data, DRF APIs and project improvements

--- zomato_clone/settings.py
+++ zomato_clone/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'zomato_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cravekart_db',
+        'USER': 'root',
+        'PASSWORD': 'Milind@9321',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
